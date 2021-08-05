642ae17b59f4739ec6ad3c3ad73b0c333903f340
Denis <dediluk@gmail.com>
2021-08-05 21:25:26 +0300

    added requirements

--- password_generator/settings.py
+++ password_generator/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-r57@+7_b!5-48vlh=9_gl*h&j53&dv=*zbv5&6&lt5oi#092b7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
